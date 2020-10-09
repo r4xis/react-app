517bab790be3b2b230362029232704e0fc44998e
Efecan Cetinkaya <r4xiss@gmail.com>
2020-10-09 16:31:21 +0300

    Delete sideAction.js

--- food-ecommerce-with-redux/src/actions/sideAction.js
+++
@@ -1,9 +0,0 @@
-import { GET_CATEGORIES } from "./types";
-import data from "../menu.json";
-
-export const getCategories = () => {
-  return {
-    type: GET_CATEGORIES,
-    payload: data.menus,
-  };
-};
