78204723624faeef260b736243da6e7b5b581f21
Efecan Cetinkaya <r4xiss@gmail.com>
2020-10-09 16:31:27 +0300

    Delete types.js

--- food-ecommerce-with-redux/src/actions/types.js
+++
@@ -1,5 +0,0 @@
-export const GET_CATEGORIES = "GET_CATEGORIES";
-export const DISCOUNTED_PRODUCTS = "DISCOUNTED_PRODUCTS";
-export const DELIVERY = "DELIVERY";
-export const DELIVERY_NUM = "DELIVERY_NUM";
-export const TOTAL_PRICE = "TOTAL_PRICE";
