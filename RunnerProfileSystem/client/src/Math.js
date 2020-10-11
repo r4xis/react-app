45fac05216a3ed250d003c8148ffa6e78bd8eaea
Efecan Cetinkaya <r4xiss@gmail.com>
2020-10-11 17:43:29 +0300

    Update Math.js

--- RunnerProfileSystem/client/src/Math.js
+++ RunnerProfileSystem/client/src/Math.js
@@ -2,8 +2,13 @@ import { create, all } from "mathjs";
 
 const Math = (distance, total_time) => {
   const math = create(all);
-  let div = math.divide(total_time, distance);
-  return math.multiply(div, 1000).toString().substring(0, 4);
+  let pace = math.divide(total_time, distance);
+  pace = math.multiply(pace, 1000);
+  let min = parseInt(pace).toString();
+  let sec = pace - min;
+  sec = math.multiply(60, sec).toString().substring(0, 2);
+  pace = parseFloat(min + "." + sec);
+  return pace;
 };
 
 export default Math;
