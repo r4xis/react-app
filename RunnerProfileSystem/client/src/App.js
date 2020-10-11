389d10eb712cf921dc1bad2937aeee601b5dc13e
Efecan Cetinkaya <r4xiss@gmail.com>
2020-10-11 03:38:11 +0300

    Update App.js

--- RunnerProfileSystem/client/src/App.js
+++ RunnerProfileSystem/client/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import DataProcess from "./components/tables/DataProcess";
 
 const App = () => {
   return (
     <div id="App">
+      <Navbar />
       <DataProcess />
+      <Footer />
     </div>
   );
 };
